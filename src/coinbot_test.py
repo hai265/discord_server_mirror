2f56ef37d028333bbec674ec17ca7ad53d399b26
Hai Nguyen <cognation100@gmail.com>
2022-01-08 13:14:16 -0500

    move commands to slackbot.py

--- src/coinbot_test.py
+++ src/coinbot_test.py
@@ -1,14 +1,13 @@
 from slack import WebClient
-from starterbot import CoinBot
+from slackbot import SlackBot
 import os
 
-slack_web_client = WebClient(token=os.environ.get("SLACK_TOKEN"))
 
-coin_bot = CoinBot("#random")
-
-message = coin_bot.get_message_payload()
+slackBot = SlackBot("#random")
+slackBot.create_channel("random")
+message = slackBot.get_message_payload("hi","random")
 
 # Post the onboarding message in Slack
-slack_web_client.chat_postMessage(**message)
-slack_web_client.chat_postMessage(**message,icon_url="https://cdn.discordapp.com/avatars/113756127707136002/2fcccde9b88abab6087b65ff68c10bd6.png?size=40", username="diffname")
+slackBot.postMessage("Hi","random")
+# client.chat_postMessage(**message,icon_url="https://cdn.discordapp.com/avatars/113756127707136002/2fcccde9b88abab6087b65ff68c10bd6.png?size=40", username="diffname")
 # 
---
+++ src/slackbot.py
@@ -0,0 +1,48 @@
+# import the random library to help us generate the random numbers
+import random
+from slack import WebClient
+import os
+# Create the CoinBot Class
+class SlackBot:
+    # Create a constant that contains the default text for the message
+    MESSAGE_BLOCK = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "Sure! Flipping a coin....\n\n"
+            ),
+        },
+    }
+    # The constructor for the class. It takes the channel name as the a
+    # parameter and then sets it as an instance variable
+    def __init__(self, channel : str):
+        print("Slack bot initialized")
+        self.channel = channel
+        self.client = WebClient(token=os.environ.get("SLACK_TOKEN"))
+
+    # Generate a random number to simulate flipping a coin. Then return the
+    # crafted slack payload with the coin flip message.
+    def generate_slack_message(self, text : str):
+        return {"type": "section", "text": {"type": "mrkdwn", "text": text}},
+
+    # Craft and return the entire message payload as a dictionary.
+    def get_message_payload(self, text : str, channelName : str):
+        return {
+            "channel": channelName,
+            "blocks": [
+                *self.generate_slack_message(text),
+            ],
+        }
+    def postMessage(self,msg : str, channel : str):
+        message = self.get_message_payload(msg,channel)
+        # Post the onboarding message in Slack
+        self.client.chat_postMessage(**message)
+    def create_channel(self,channel_name : str):
+        response = self.client.conversations_list()
+        conversations = response["channels"]
+        for conversation in conversations:
+            if conversation["name"] == channel_name:
+                print("Channel name already exists")
+                return
+        
